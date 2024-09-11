ee88ba2a9fefafc7c1faeb8cfc7d02e30897ba30
Nikhil-r21 <rnikhilvignesh21@gmail.com>
2024-09-11 17:36:06 +0530

    write csv file using file handling

---
+++ File_Processing/Write_csv_file.py
@@ -0,0 +1,14 @@
+import csv
+
+with open('./File_Processing/Data/people.csv', 'a') as csvfile:
+    writer = csv.writer(csvfile)
+    csvdata = (5, 'Luffy', 'Tokyo')
+    writer.writerow(csvdata)
+
+
+with open('./File_Processing/Data/numbers.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['x', 'x**2', 'x**3', 'x**4'])
+    for x in range(1, 101):
+        writer.writerow([x, x**2, x**3, x**4])
+
