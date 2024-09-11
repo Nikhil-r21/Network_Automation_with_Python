17f421e3c1db93cbe20c5ce8e393c22e449f5354
Nikhil-r21 <rnikhilvignesh21@gmail.com>
2024-09-11 17:03:08 +0530

    File Processing project using split()

---
+++ File_Processing/Project_File_Processing.py
@@ -0,0 +1,11 @@
+with open('./File_Processing/Data/devices.txt') as f:
+    content = f.read().splitlines()
+    print(content)
+    devices = list()
+    for line in content[1:]:
+        devices.append(line.split(':'))
+
+print(devices)
+
+for device in devices:
+    print(f"pinging {device[1]}")
